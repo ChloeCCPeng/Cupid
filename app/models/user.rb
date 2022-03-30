462733c045694e3a761e7f34f48bbe7e0e0c430c
Evan Chernicky <echernicky@gmail.com>
2022-03-30 16:11:17 -0400

    login screen

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,13 +5,13 @@ class User < ActiveRecord::Base
     validates :username, presence: true
     validates :username, uniqueness: true
 
-    #this user can have many liked users
-    has_many :liked_users, foreign_key: :liker_id, class_name: "Match"
-    has_many :liked, through: :liked_users
-    
-    #this users can also like many users
-    has_many :liking_users, foreign_key: :liked_id, class_name: "Match"
-    has_many :likers, through: :liking_users
+        #this user can have many followed users
+        has_many :liked_users, foreign_key: :liker_id, class_name: "Match"
+        has_many :liked, through: :liked_users
+        
+        #this users can also like many users
+        has_many :liking_users, foreign_key: :liked_id, class_name: "Match"
+        has_many :likers, through: :liking_users
 
     def update_user
         
--- dating-app/src/Components/App.js
+++ dating-app/src/Components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import '../styles/App.css';
 import Home from './Home'
+import Login from './Login'
 
 
 function App() {
@@ -9,6 +10,9 @@ function App() {
     <Router>
       <div className="App">
       <Switch>
+      <Route exact path="/login">
+          <Login />
+        </Route>
         <Route exact path="/">
           <Home />
         </Route>
@@ -16,49 +20,6 @@ function App() {
       </div>
     </Router>
   );
-}
-
-function DeleteUser({ user, onDeleteUser }) {
-  function handleDeleteClick() {
-    fetch(`http://localhost:9292/reviews/${user.id}`, {
-      method: "DELETE",
-    })
-      .then((r) => r.json())
-      .then((deletedUser) => onDeleteUser(deletedUser));
-  }
-
-  return (
-    <div>
-      <p>You've deleted the account. Good bye!</p>
-    </div>
-  );
-}
-
-function updateUser({ user, onUpdateUser }) {
-  const [user, setUser] = useState("");
-
-  function handleSubmit(e) {
-    e.preventDefault();
-    fetch(`http://localhost:9292/reviews/${user.id}`, {
-      method: "PATCH",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        name: name,
-        bio: bio,
-        hobby: hobby,
-        preference: preference,
-        age: age,
-        picture: picture,
-        location: location
-      }),
-    })
-      .then((r) => r.json())
-      .then((updatedUser) => onUpdateUser(updatedUser));
-  }
-
-  return <form onSubmit={handleSubmit}>{/* controlled form code here*/}</form>;
 }
 
 export default App;
