8ad0a011312ced2083e7f87fd8b9e8dc3089708c
Evan Chernicky <echernicky@gmail.com>
2022-03-31 17:25:32 -0400

    added link to profile page

--- dating-app/src/Components/NavBar.js
+++ dating-app/src/Components/NavBar.js
@@ -1,11 +1,29 @@
-import React from 'react';
-import profile_icon from '../assets/User.svg'
+import React, {useEffect, useState} from 'react';
+import { Link } from "react-router-dom";
 import message_icon from '../assets/Menu.svg'
 
 function NavBar() {
+
+    const [profile, setProfile] = useState([])
+
+    useEffect(() => {
+
+        const userID = localStorage.getItem('User ID')
+    
+        fetch(`http://localhost:9292/profile/${userID}`, {
+          headers : { 
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+           }
+        })
+        .then(resp => resp.json())
+        .then(userData => setProfile(userData))
+    
+      },[])
+
     return (
     <div className="h-24 flex place-content-between">
-        <img src={profile_icon} className="rounded-full" alt="User icon"/>
+        <Link to="/profile"><img src={profile.picture} className="rounded-full w-100 object-cover item-scale" style={{width: "100px", height: "100px"}} alt="User icon"/></Link>
         <img src={message_icon} alt="Message icon"/>
     </div>
 
